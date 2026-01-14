c1f3ee3e30c32d0601d19eba04ccc63405c5b743
jyssy <jessel@phantomlandscapes.com>
2026-01-14 13:13:49 -0500

    adding uv dependency skeleton info

---
+++ database/pg_dump_cms.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DATE=`date +'%s'`
+FILE=dump/cms.dump.${DATE}
+echo "pg_dump to: ${FILE}"
+
+pg_dump -a -p 5434 -U cms_django \
+    -t cms.availability -t cms.site -t cms.staff -t cms.support \
+    -t cms.service -t cms.host -t cms.link -t cms.logentry \
+    -t cms.event -t cms.hosteventlog -t cms.hosteventstatus \
+    cms1 >${FILE}
+echo "Dump complete."
+echo "Manually execute:"
+echo "DROP OWNED BY cms_django;"
+echo "CREATE SCHEMA cms AUTHORIZATION cms_django;"
