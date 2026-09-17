7bec85171e58a2b17b6442afc00698f00cb10f8d
jyssy <jessel@phantomlandscapes.com>
2026-09-17 15:17:22 -0400

    see changelog v0.7.11 about url updating

--- operations_portalcms_django/integration_news/urls.py
+++ operations_portalcms_django/integration_news/urls.py
@@ -5,7 +5,7 @@ from . import workflow
 app_name = 'integration_news'
 
 urlpatterns = [
-    path('integration-news/', views.integration_news, name='integration_news'),
+    path('integration_news', views.integration_news, name='integration_news'),
     path('integration-news/add/', views.add_integration_news, name='add_integration_news'),
     path('integration-news/update/<int:pk>/', views.update_integration_news, name='update_integration_news'),
     path('integration-news/<int:pk>/publish/', workflow.publish_integration_news, name='publish_integration_news'),
